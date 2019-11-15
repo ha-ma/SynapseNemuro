7fe525073a7e90e53358d747b1b916d22d1c454f
ha-ma <01yhamaya05@gmail.com>
2019-11-15 18:18:53 +0900

    Complete front MV

--- inc-front/front-MV.php
+++ inc-front/front-MV.php
@@ -1,16 +1,18 @@
 <section class="front__MV">
   <div class="front__MV--container">
-    <h2 class="front__MV__heading"><span class="front__MV__heading--first">LEAVE</span><br><span class="front__MV__heading--second">NO ONE BEHIND</span><br><span class="front__MV__heading--third">IN NEMURO AREA</span></h2>
+    <h2 class="front__MV__heading enheading"><span class="front__MV__heading--first enheading">LEAVE</span><br><span class="front__MV__heading--second">NO ONE BEHIND</span><br><span class="front__MV__heading--third">IN NEMURO AREA</span></h2>
     <p class="front__MV__subHeading">ねむろのだれひとり、とりこのさない。</p>
 
-    <div class="front__MV__btns">
+    <div class="front__MV__btns d-flex flex-wrap justify-content-center">
       <a href="#front__SDGsEntry" class="front__MV__btn front__MV__btn--entryform btn btn--grad">SDGsカードゲームへの応募</a>
       <a href="<?php bloginfo( 'url' ); ?>/contact" class="front__MV__btn front__MV__btn--contactform btn btn--outlined">お問い合わせ</a>
     </div> <!-- /.front__MV__btns -->
 
-    <div class="front__MV__illustration">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/students.png" alt="High school students" class="front__MV__illustration__img">
-    </div> <!-- /.front__MV__illustration -->
-
   </div> <!-- /.front__MV--container -->
+
+  <div class="front__MV__illustration">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/students.png" alt="High school students" class="front__MV__illustration__img">
+  </div> <!-- /.front__MV__illustration -->
+
+  <img src="<?php echo get_template_directory_uri(); ?>/images/doto.png" alt="Eastern Area or Hokkaido" class="front__MV__doto">
 </section> <!-- /.front__MV -->
