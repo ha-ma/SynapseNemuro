e2a9fbc8a91387e5d8e073b656b38b911c23bbb1
ha-ma <01yhamaya05@gmail.com>
2019-11-19 13:43:00 +0900

    Fix pages

--- inc-front/front-about.php
+++ inc-front/front-about.php
@@ -1,8 +1,16 @@
 <section class="front__about">
   <div class="front__about--container">
     <h2 class="front__about__heading">CONNECT LIKE SYNAPSE</h2>
-    <h3 class="front__about__subHeading">シナプスのようにつながる</h4>
+    <h3 class="front__about__subHeading">シナプスのようにつながる</h3>
 
-    <a class="front__about__contactbtn btn btn--solidNavy" href="<?php bloginfo('url'); ?>/contact">お問い合わせ</a>
+    <div class="front__about__desc">
+      <h4 class="front__about__desc__secheading">シナプスねむろについて</h4>
+      <p class="front__about__desc__secbody">「自分たちの地域の良さを幼い頃から知ることができれば、もっと地域を好きになって、大きな夢を持って育っていける」という思いがつながり、根室を中心とした有志が活動しています。</p>
+
+      <h4 class="front__about__desc__secheading">きっかけは、SDGs（えす・でぃ・じー・ず）</h4>
+      <p class="front__about__desc__secbody">今回は、中学生・高校生・一般を対象としての、SDGsカードゲームをおこないます。<br>SDGsカードゲームは、次世代の若者がゲームを通して環境・地域・経済のつながりを理解し、楽しく学びながらSDGsを理解し、『持続可能な地域づくり』を『自分ゴト化』するきっかけが生まれるチームで行うゲームです。</p>
+    </div> <!-- /.front__about__desc -->
+
+    <a class="front__about__contactbtn btn btn--solidWhite" href="<?php bloginfo('url'); ?>/contact">お問い合わせ</a>
   </div> <!-- /.front__about--container -->
 </section> <!-- /.front__about__members -->
